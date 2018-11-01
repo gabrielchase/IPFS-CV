360070cf38a3b3a794c914d4d24c273c64d599a8
gabrielchase <gchase.patron@gmail.com>
2018-11-01 21:12:32 +0800

    Convert year and month dates

--- utils.js
+++ utils.js
@@ -13,5 +13,9 @@ module.exports = {
             reason: err.message
         }
         res.json(obj)
+    },
+    convertYearMonth: (date) => {
+        const [ year, month ] = date.split('-')
+        return new Date(parseInt(year), parseInt(month)).toString()
     }
 }
