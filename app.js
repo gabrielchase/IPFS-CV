e5e32fa1d49fb10e259890c0fb6e2b41436fadd0
gabrielchase <gchase.patron@gmail.com>
2018-12-09 15:05:27 +0800

    Change port

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const ipfsAPI = require('ipfs-api')
 const ipfs = ipfsAPI('ipfs.infura.io', '5001', {protocol: 'https'})
 
 const config = require('./config/config')
+const PORT = process.env.PORT || config.PORT
 
-// "mongodb://admin:admin11@ds159180.mlab.com:59180/thesis-dev-db",
 console.log('DB_URL: ', config.DB_URL)
+console.log('PORT: ', PORT)
+
+// "mongodb://admin:admin11@ds159180.mlab.com:59180/thesis-dev-db"
 mongoose.connect(config.DB_URL, { useNewUrlParser: true })
 mongoose.Promise = global.Promise
 
@@ -24,6 +27,6 @@ app.use(cors())
 require('./routes/auth_routes')(app)
 require('./routes/user_routes')(app, ipfs)
 
-app.listen(config.PORT, () => {
-    console.log(`Server running on ${config.PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
 })
